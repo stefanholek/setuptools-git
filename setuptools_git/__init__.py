260cb1b6c2d2b4d5079476467e8c697b2e9de43f
Stefan H. Holek <stefan@epy.co.at>
2017-01-27 00:59:03 +0100

    Strip symlink support in exchange for massive performance increase

--- setuptools_git/__init__.py
+++ setuptools_git/__init__.py
@@ -2,19 +2,12 @@
 A hook into setuptools for Git.
 """
 import sys
-import os
-import posixpath
 
-from os.path import realpath, join
 from subprocess import PIPE
 
 from setuptools_git.utils import check_output
 from setuptools_git.utils import b
-from setuptools_git.utils import posix
 from setuptools_git.utils import fsdecode
-from setuptools_git.utils import hfs_quote
-from setuptools_git.utils import compose
-from setuptools_git.utils import decompose
 from setuptools_git.utils import CalledProcessError
 
 
@@ -33,7 +26,7 @@ def ntfsdecode(path):
     return path
 
 
-def gitlsfiles(dirname=''):
+def listfiles(dirname=''):
     # NB: Passing the '-z' option to 'git ls-files' below returns the
     # output as a blob of null-terminated filenames without canonical-
     # ization or use of double-quoting.
@@ -47,57 +40,19 @@ def gitlsfiles(dirname=''):
     # '"pyramid/tests/fixtures/static/h\\303\\251h\\303\\251.html"'
     #
     # for each file.
-    res = set()
-
     try:
-        topdir = check_output(
-            ['git', 'rev-parse', '--show-toplevel'], cwd=dirname or None,
-            stderr=PIPE).strip()
-
-        if sys.platform == 'win32':
-            cwd = ntfsdecode(topdir)
-        else:
-            cwd = topdir
-
         filenames = check_output(
-            ['git', 'ls-files', '-z'], cwd=cwd, stderr=PIPE)
+            ['git', 'ls-files', '-z'], cwd=dirname or None, stderr=PIPE)
     except (CalledProcessError, OSError):
         # Setuptools mandates we fail silently
-        return res
+        return
 
     for filename in filenames.split(b('\x00')):
         if filename:
-            filename = posixpath.join(topdir, filename)
-            if sys.platform == 'darwin':
-                filename = hfs_quote(filename)
             if sys.platform == 'win32':
-                filename = ntfsdecode(filename)
+                yield ntfsdecode(filename)
             else:
-                filename = fsdecode(filename)
-            if sys.platform == 'darwin':
-                filename = decompose(filename)
-            res.add(filename)
-    return res
-
-
-def listfiles(dirname=''):
-    git_files = gitlsfiles(dirname)
-    if not git_files:
-        return
-
-    cwd = realpath(dirname or os.curdir)
-    prefix_length = len(cwd) + 1
-
-    if sys.version_info >= (2, 6):
-        walker = os.walk(cwd, followlinks=True)
-    else:
-        walker = os.walk(cwd)
-
-    for root, dirs, files in walker:
-        for file in files:
-            filename = join(root, file)
-            if posix(realpath(filename)) in git_files:
-                yield filename[prefix_length:]
+                yield fsdecode(filename)
 
 
 if __name__ == '__main__':
@@ -107,7 +62,7 @@ if __name__ == '__main__':
         dirname = ''
     for filename in listfiles(dirname):
         try:
-            print(compose(filename))
+            print(filename)
         except UnicodeEncodeError:
             print(repr(filename)[1:-1])
 
